9e7a88f97d5fb777fa85ec2e99f355986935834a
Darcy Laycock <sutto@thefrontiergroup.com.au>
2011-01-23 19:33:38 +0800

    Add in the repository model bare bone structure

---
+++ db/migrate/20110123112630_create_repositories.rb
@@ -0,0 +1,17 @@
+class CreateRepositories < ActiveRecord::Migration
+  def self.up
+    create_table :repositories do |t|
+      t.string :name, :null => false
+      t.text   :description
+      t.string :identifier, :null => false
+      t.string :cached_slug, :null => false
+      t.belongs_to :owner, :polymorphic => true
+      t.timestamps
+    end
+    add_index :repositories, [:owner_type, :owner_id, :cached_slug]
+  end
+
+  def self.down
+    drop_table :repositories
+  end
+end
