bfd427914273bb2f72322aa9b22802c0ee4a6304
Darcy Laycock <sutto@thefrontiergroup.com.au>
2011-01-23 17:39:46 +0800

    Initial check in of a new rails app

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rimmer
+
+> Do you? Well, thank you. No-one's ever said I was charming before. They've said, "Rimmer, you're a total git." But never charming, no.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rimmer::Application.config.secret_token = '1236f5ad496f6b828d1735cc86a224183cb98fa3e5cb480fcda0c9f38d4cb4a70fa52370020d164fc437e0222aa4a4c8bbd0cd29a43c3499788796ea8c167769'
